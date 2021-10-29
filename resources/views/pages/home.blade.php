e629b3881b58885104b0d71da4cda7d530edcbc8
damimoses <92752166+damimoses@users.noreply.github.com>
2021-10-29 12:57:15 -0500

    first commit

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,252 @@
+@extends('layout.app')
+
+@section('content')
+    
+    <!-- Start Banner -->
+    <div class="ulockd-home-slider">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="pogoSlider" id="js-main-slider">
+                    <div class="pogoSlider-slide" style="background-image:url(images/dark.jpg);">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="slide_text white_fonts">
+                                        <h3 style="font-family: Copperplate, Papyrus, fantasy;">The best and most trusted website to trade your gift cards and Bitcoin</h3>
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                <!-- .pogoSlider -->
+            </div>
+        </div>
+    </div>
+    <!-- End Banner -->
+    
+	<!-- section -->
+    <div class="section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="full text_align_right_img">
+                        <img src="images/img1.png" alt="#" />
+                    </div>
+                </div>
+                <div class="col-md-6 layout_padding">
+                    <div class="full paddding_left_15">
+                        <div class="heading_main text_align_left">
+						   <h2><span class="theme_color">Welcome</span> To Exchange</h2>	
+                        </div>
+                    </div>
+                    <div class="full paddding_left_15">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                    </div>
+                    <div class="full paddding_left_15">
+                        <a class="main_bt" href="#">About more ></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+	<!-- end section -->
+    <!-- section -->
+    <div class="section layout_padding">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="full">
+                        <div class="heading_main text_align_center">
+                           <h2><span class="theme_color"></span>Services</h2>    
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-3 col-sm-6 col-xs-12">
+                    <div class="full services_blog">
+                       <img class="img-responsive" src="images/s1.png" alt="#" />
+                       <h4>Safe & Secure</h4>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6 col-xs-12">
+                    <div class="full services_blog">
+                        <img class="img-responsive" src="images/s2.png" alt="#" />
+                        <h4>Mobile Apps</h4>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6 col-xs-12">
+                    <div class="full services_blog">
+                        <img class="img-responsive" src="images/s3.png" alt="#" />
+                        <h4>Wallet</h4>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6 col-xs-12">
+                    <div class="full services_blog">
+                        <img class="img-responsive" src="images/s4.png" alt="#" />
+                        <h4>Experts Support</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="row margin-top_30">
+                <div class="col-sm-12">
+                    <div class="full">
+                        <div class="center">
+                            <a class="main_bt" href="#">About more ></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- end section -->
+    <!-- section -->
+    <div class="section white_fonts" style="background: #2a2a2a;">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6" style="background: #fff;">
+                    <div class="full text_align_right_img">
+                        <img src="images/img2.png" alt="#" />
+                    </div>
+                </div>
+                <div class="col-md-6 layout_padding">
+                    <div class="full paddding_left_15">
+                        <div class="heading_main text_align_left">
+                           <h2><span class="theme_color">Apply for</span> Exchange</h2>    
+                        </div>
+                    </div>
+                    <div class="full paddding_left_15">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                    </div>
+                    <div class="full paddding_left_15">
+                        <a class="main_bt" href="#">Exchange ></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- end section -->
+	<!-- section -->
+    <div class="section layout_padding about_bg">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="full paddding_left_15">
+                        <div class="heading_main text_align_left">
+                           <h2>About</h2>    
+                        </div>
+                    </div>
+                    <div class="full paddding_left_15">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                    </div>
+                    <div class="full paddding_left_15">
+                        <a class="main_bt" href="#">Read More ></a>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="full text_align_right_img">
+                        <img src="images/img3.png" alt="#" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- end section -->
+	<!-- section -->
+    <div class="section layout_padding padding_top_0">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="full">
+                        <div class="heading_main text_align_center">
+                           <h2><span class="theme_color"></span>News</h2>    
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4 col-sm-6 col-xs-12">
+                    <div class="full news_blog">
+                       <img class="img-responsive" src="images/b1.png" alt="#" />
+                       <div class="overlay"><a class="main_bt transparent" href="#">View</a></div>
+                       <div class="blog_details">
+                         <h3>Bitcoin News</h3>
+                         <p>pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                       </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-6 col-xs-12">
+                    <div class="full news_blog">
+                        <img class="img-responsive" src="images/b2.png" alt="#" />
+                        <div class="overlay"><a class="main_bt transparent" href="#">View</a></div>
+                       <div class="blog_details">
+                         <h3>Ethereum News</h3>
+                         <p>pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                       </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-6 col-xs-12">
+                    <div class="full news_blog">
+                        <img class="img-responsive" src="images/b3.png" alt="#" />
+                        <div class="overlay"><a class="main_bt transparent" href="#">View</a></div>
+                       <div class="blog_details">
+                         <h3>Light News</h3>
+                         <p>pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                       </div>
+                    </div>
+                </div>
+             </div>
+        </div>
+    </div>
+    <!-- end section -->
+    <!-- section -->
+    <div class="section layout_padding padding_top_0">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="full">
+                        <div class="heading_main text_align_center">
+                           <h2><span class="theme_color"></span>Contact</h2>    
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- end section -->
+    <!-- contact_form -->
+    <div class="section contact_form">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-sm-12 offset-lg-3">
+                    <div class="full">
+                        <form class="contact_form_inner" action="#">
+                            <fieldset>
+                                <div class="field">
+                                    <input type="text" name="name" placeholder="Your name" />
+                                </div>
+                                <div class="field">
+                                    <input type="email" name="email" placeholder="Email" />
+                                </div>
+                                <div class="field">
+                                    <input type="text" name="phone_no" placeholder="Phone number" />
+                                </div>
+                                <div class="field">
+                                    <textarea placeholder="Message"></textarea>
+                                </div>
+                                <div class="field center">
+                                    <button>SEND</button>
+                                </div>
+                            </fieldset>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- end contact_form -->
+    <!-- end section -->
+
+@endsection
