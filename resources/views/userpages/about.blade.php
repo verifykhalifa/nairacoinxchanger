f7ae0fc7d477b4ed3dade8384e55705db64ea68a
damimoses <92752166+damimoses@users.noreply.github.com>
2022-01-02 11:59:38 -0600

    finishing

--- resources/views/userpages/about.blade.php
+++ resources/views/userpages/about.blade.php
@@ -13,7 +13,7 @@
             </div>
             <div class="col-lg-6 col-md-12">
                 <div class="abt-text">
-                    <p class="top-sub">Since Year 1999</p>
+                    
                     <h2>We are <span class="orange-text">Nairacoinxchange</span></h2>
                     <p>Nairacoinxchange is an alternative payment solutions company that facilitates the easy use of cryptocurrencies for everyday transactions. By harnessing the intrinsic power of Blockchain technology, </p>
                     <p>Nairacoinxchange is set to make Crypto easy for mankind, hereby creating a new and sustainable path for the present and future generations.</p>
