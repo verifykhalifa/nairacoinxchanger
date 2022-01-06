5fc7e6e20c23d520513fc56d6c2c1c71a7dda586
ayodvr <ayodejiadekunle@gmail.com>
2022-01-06 13:58:32 +0100

    Inv

--- resources/views/purchase/show.blade.php
+++ resources/views/purchase/show.blade.php
@@ -1,14 +1,30 @@
-@extends('layout.dash')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <!--  This file has been downloaded from bootdey.com @bootdey on twitter -->
+    <!--  All snippets are MIT license http://bootdey.com/license -->
+    <title>Vertical Timeline with lines - Bootdey.com</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" rel="stylesheet">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/5.3.45/css/materialdesignicons.min.css" integrity="sha256-nwNjrH7J9zS/Ti4twtWX7OsC5QdQHCIKTv5cLMsGo68=" crossorigin="anonymous" />
 
-@section('content')
-    
-<div class="content-body">
-    <div class="container-fluid">
-        <div class="row" >
-            <div class="d-flex justify-content-center align-items-center">
-                <div class="col-md-9">
-                    <div class="card">
-                        <div class="card-body">
+<div class="container">
+<div class="row">
+<div class="col-lg-12">
+    <div class="card">
+        <div class="card-body">
+            <div class="card-title mb-5 text-center">
+                <img  src="../assets/img/nairacoinr.png" style="width: 80px; height: 95px;">
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-lg-12">
+                    <ul class="verti-timeline list-unstyled" dir="ltr">
+                        <li class="event-list">
                             <form action="{{route('sellMail')}}" method="POST">
                                 @csrf
                                 <div class="text-center">
@@ -18,56 +34,37 @@
                                     @endif
                                 </div>
                                 <br>
-                            <div class="buyer-seller">
-                                <div class="d-flex justify-center">
-                                    <div class="buyer-info">
-                                        
-                                        <div class="d-flex align-items-center">
-                                            <div class="flex-grow-1">
-                                                <h3>Order Id: {{ $purchase->orderId }}</h3>
-                                            </div>
-                                        </div>
+                                <div class="event-content p-4">
+                                    <h5 class="mt-0 mb-3 font-18">  Order Id: {{ $purchase->orderId }}</h5>
+                                    <div>
+                                        <h5><span class="text-danger">Notice:</span></h5>
+                                        <p><span class="text-danger">Please Pay directly from your registerd bank account, or else it will not be treated.</span></p>
                                     </div>
-                                    
-                                </div>
-                                <div class="table-responsive">
-                                    <table class="table">
-                                        <tbody>
-                                            <tr>
-                                                <td><span class="text-danger">Notice </span></td>
-                                                <td align="center"><span class="text-danger">Please Pay directly from your registerd bank account, or else it will not be treated.</span></td>
-                                            </tr>
-                                          
-                                            
-                                            <tr>
-                                                <td>Amount To Send</td>
-                                                <td align="center"><span> &#x20A6;</span>@money($purchase->total)</td>
-                                            </tr>
-                                            <tr>
-                                                <td>Account To Fund</td>
-                                                <td align="center">Bank Name: {{ $adminBank->bankname}}<br>Acct Number: {{ $adminBank->acctnumber}}<br>Acct Name: {{ $adminBank->firstname}}&nbsp;{{ $adminBank->lastname}}</td>
-                                            </tr>
-                                            <tr>
-                                                <td></td>
-                                                <td align="center">Write "{{ $purchase->orderId }} order" in the remark or reference when sending.</td>
-                                            </tr>
-                                            <tr>
-                                                <td><span class="text-danger">NOTE </span></td>
-                                                <td align="center"><span class="text-danger">Please DO NOT Write BITCOIN or ETHERIUM or any related words on narration ( Bank will block both sender and receiver account). Please Comply</span></td>
-                                            </tr>
-                                            <tr>
-                                                <td></td>
-                                                <td align="center">" <span>$</span>{{ $purchase->value }} " will be sent to " {{ $purchase->btcaddress }} " as soon as we confirm your Payment</td>
-                                            </tr>
-                                            <tr>
-                                                <td></td>
-                                                <td align="center">Click on "CONFIRM" after payment. We will recieve a notification and check the receipt of funds</td>
-                                            </tr>
-                                        
-                                          
-                                            
-                                        </tbody>
-                                    </table>
+                                    <div>
+                                        <h5><span>Amount To Send</span></h5>
+                                        <p><span> &#x20A6;</span>@money($purchase->total)</p>
+                                    </div>
+                                    <div>
+                                        <h5><span>Account To Fund</span></h5>
+                                        <p><strong>Bank Name: {{ $adminBank->bankname}}<br>Acct Number: {{ $adminBank->acctnumber}}<br>Acct Name: {{ $adminBank->firstname}}&nbsp;{{ $adminBank->lastname}}</strong></p>
+                                    </div>
+                                    <br>
+                                    <div>
+                                        <h5><span></span></h5>
+                                        <p><span>Write "{{ $purchase->orderId }} order" in the remark for reference when sending.</span></p>
+                                    </div>
+                                    <div>
+                                        <h5><span class="text-danger">NOTE </span></h5>
+                                        <p><span class="text-danger">Please DO NOT Write BITCOIN or ETHEREUM or any related words on narration ( Bank will block both sender and receiver account),  Please Comply!</span></p>
+                                    </div>
+                                    <div>
+                                        <h5></h5>
+                                        <p><b>Your transaction will be made as soon as we confirm your Payment!</b></p>
+                                    </div>
+                                    <div>
+                                        <h5></span></h5>
+                                        <p>Click on "CONFIRM" after payment. We will recieve a notification and check the receipt of funds</p>
+                                    </div>
                                     <div class="d-flex justify-content-center align-items-center">
                                         <input type="text" name="orderId" value="{{ $purchase->orderId }}" hidden='hidden'>
                                         <input type="text" name="total" value="{{ $purchase->total }}" hidden='hidden'>
@@ -77,20 +74,103 @@
                                         <input type="text" name="firstname" value="{{ $adminBank->firstname }}" hidden='hidden'>
                                         <input type="text" name="lastname" value="{{ $adminBank->lastname }}" hidden='hidden'>
                                         <input type="text" name="email" value="{{ $authEmail }}" hidden='hidden'>
-                                        <input type="submit" class="btn btn-primary px-4 me-3" value="Confirm"> 
+                                        <input type="submit" class="btn btn-success px-4 me-3" value="Confirm"> 
                                     </div>
                                 </div>
-                            </div>
-                        </form>
-                        </div>
-                    </div>
+                            </form>
+                        </li>
+                    </ul>
                 </div>
             </div>
+            <!-- end row -->
         </div>
-
-    
     </div>
+    <!-- end card -->
+</div>
 </div>
 
+<style type="text/css">
+body{
+    background:#3a3361;
+    margin-top:20px;
+}
+.verti-timeline {
+    /* border-left: 3px dashed #e9ecef; */
+    margin: 0 10px;
+}
+.verti-timeline .event-list {
+    position: relative;
+    padding: 40px 15px 40px;
+    /* border-top: 3px solid #e9ecef; */
+}
+.verti-timeline .event-list .event-date {
+    position: absolute;
+    right: 0;
+    top: -2px;
+    padding: 12px;
+    color: #fff;
+    border-radius: 0 0 7px 7px;
+    text-align: center;
+}
+.verti-timeline .event-list .event-content {
+    position: relative;
+    border: 2px solid #e9ecef;
+    border-radius: 7px;
+    text-align: center;
+}
+.verti-timeline .event-list .event-content::before {
+    content: "";
+    position: absolute;
+    height: 40px;
+    width: 75%;
+    top: -42px;
+    left: 0;
+    right: 0;
+    margin: 0 auto;
+    /* border-left: 12px double #e9ecef;
+    border-right: 12px double #e9ecef; */
+}
+.verti-timeline .event-list .timeline-icon {
+    position: absolute;
+    left: -14px;
+    top: -10px;
+}
+.verti-timeline .event-list .timeline-icon i {
+    display: inline-block;
+    width: 23px;
+    height: 23px;
+    line-height: 23px;
+    font-size: 20px;
+    border-radius: 50%;
+    text-align: center;
+    color: #fff;
+}
+.verti-timeline .event-list:last-child {
+    padding-bottom: 0;
+}
+@media (min-width: 769px) {
+    .verti-timeline {
+        margin: 0 60px;
+    }
+    .verti-timeline .event-list {
+        padding: 40px 90px 60px 40px;
+    }
+}
+@media (max-width: 576px) {
+    .verti-timeline .event-list .event-date {
+        right: -20px;
+    }
+}
+.card {
+    border: none;
+    margin-bottom: 24px;
+    -webkit-box-shadow: 0 0 13px 0 rgba(236,236,241,.44);
+    box-shadow: 0 0 13px 0 rgba(236,236,241,.44);
+}
+</style>
 
-@endsection
+<script type="text/javascript">
+
+</script>
+</body>
+</html>
--- resources/views/sale/show.blade.php
+++ resources/views/sale/show.blade.php
@@ -1,14 +1,30 @@
-@extends('layout.dash')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <!--  This file has been downloaded from bootdey.com @bootdey on twitter -->
+    <!--  All snippets are MIT license http://bootdey.com/license -->
+    <title>Vertical Timeline with lines - Bootdey.com</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" rel="stylesheet">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/5.3.45/css/materialdesignicons.min.css" integrity="sha256-nwNjrH7J9zS/Ti4twtWX7OsC5QdQHCIKTv5cLMsGo68=" crossorigin="anonymous" />
 
-@section('content')
-    
-<div class="content-body">
-    <div class="container-fluid">
-        <div class="row" >
-            <div class="d-flex justify-content-center align-items-center">
-                <div class="col-md-9">
-                    <div class="card">
-                        <div class="card-body">
+<div class="container">
+<div class="row">
+<div class="col-lg-12">
+    <div class="card">
+        <div class="card-body">
+            <div class="card-title mb-5 text-center">
+                <img  src="../assets/img/nairacoinr.png" style="width: 80px; height: 95px;">
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-lg-12">
+                    <ul class="verti-timeline list-unstyled" dir="ltr">
+                        <li class="event-list">
                             <form action="{{route('buyMail')}}" method="POST">
                                 @csrf
                                 <div class="text-center">
@@ -17,98 +33,154 @@
                                     {{session('success')}}</div>
                                     @endif
                                 </div>
-                            <div class="buyer-seller">
-                                <div class="d-flex justify-center">
-                                    <div class="buyer-info">
-                                        <div class="d-flex align-items-center">
-                                            
-                                            <div class="flex-grow-1">
-                                                <h3>Order Id: {{ $sale->orderId }}</h3>
-                                            </div>
-                                        </div>
+                                <div class="event-content p-4">
+                                    <h5 class="mt-0 mb-3 font-18">  Order Id: {{ $sale->orderId }}</h5>
+                                    <div>
+                                        <h5><span class="text-danger">Notice:</span></h5>
+                                        <p><span class="text-danger">Send with Priority fees or high charges to avoid delay payment from {{ $sale->rate }} Network</span></p>
                                     </div>
-                                    
-                                </div>
-                                <div class="table-responsive">
-                                    <table class="table">
-                                        <tbody>
-                                            <tr>
-                                                <td><span class="text-danger">Notice </span></td>
-                                                <td align="center"><span class="text-danger">Send with Priority fees or high charges to avoid delay payment from {{ $barcode->coin }} Network</span></td>
-                                            </tr>
-                                            <tr>
-                                                <td>Processing Time</td>
-                                                <td align="center">Wallet Expire Within 60hrs (HOUR)</td>
-                                            </tr>
-                                            <tr>
-                                                <td></td>
-                                                <td align="center">Please Send <span>$</span>{{ $sale->value}} to this address <span><b>&#8595;</b></span></td>
-                                            </tr>
-                                            <tr>
-                                                <td></td>
-                                                <td><div id="container" style="height: 250px;">
-                                                    <img src="{{$barcode->barcode}}" width="250px" style="margin-left:150px;" alt="barcode" />
-                                                </div></td>
-                                            </tr>
-                                            <tr class="flex">
-                                                <td>Address</td>
-                                                <td align="center">{{$barcode->address}}</td>
-                                            </tr>
-                                            <tr>
-                                                <td>Amount To Recieve</td>
-                                                <td><span> &#x20A6;</span>@money($sale->total)</td>
-                                            </tr>
-                                            <tr>
-                                                <td> </td>
-                                                <td> This is automatic direction.
-                                                    After receiving 1 confirmation from the {{ $barcode->coin }} network, your funds move to requistes specified in the order details with prepresently ( {{ $barcode->coin }} ) to dollar($) convert Naira (#) Rate is fixed at 3 minutes recalculated {{ $barcode->coin }} network Payment will be sent to  <span><b>&#8595;</b></span></td>
-                                            </tr>
-                                            <tr>
-                                                <td>Recieving Account</td>
-                                                <td>
-                                                    <div class="text-danger">
-                                                        Bank Name: {{ $usAct['bankname'] }}<br>Acct Number: {{ $usAct['acctnumber'] }}<br>Acct Name: {{ $usAct['firstname']}}&nbsp;{{ $usAct['lastname']}}
-                                                    </div>
-                                                    
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td></td>
-                                                <td align="center">Click on "CONFIRM" after payment. We will recieve a notification and check the receipt of funds</td>
-                                            </tr>
-                                        
-                                          
-                                            
-                                        </tbody>
-                                    </table>
+                                    <div>
+                                        <h5><span>Processing Time:</span></h5>
+                                        <p><span>Wallet Expire Within 60hrs (HOUR)</span></p>
+                                    </div>
+                                    <div>
+                                        <h5><span></span></h5>
+                                        <p><strong>Please Send <span>$</span>{{ $sale->value}} to this address <span><b>&#8595;</b></span></strong></p>
+                                        <span><img src="{{$barcode->barcode}}" width="150px" alt="barcode" /></span>
+                                    </div>
+                                    <br>
+                                    <div>
+                                        <h5><span>Address:</span></h5>
+                                        <p><span>{{$barcode->address}}</span></p>
+                                    </div>
+                                    <div>
+                                        <h5><span>Amount To receive:</span></h5>
+                                        <p><span> &#x20A6;</span>@money($sale->total)</p>
+                                    </div>
+                                    <div>
+                                        <h5></h5>
+                                        <p>This is automatic direction.
+                                            After receiving 1 confirmation from the {{ $sale->rate }} network, your funds move to requistes specified in the order details with prepresently ( {{ $sale->rate }} ) to dollar($) convert Naira (&#x20A6;) Rate is fixed at 3 minutes recalculated {{ $sale->rate }} network Payment will be sent to  <span><b>&#8595;</b></span></p>
+                                    </div>
+                                    <div>
+                                        <h5><span>Recieving Account:</span></h5>
+                                        <p><b>Bank Name: {{ $usAct['bankname'] }}<br>Acct Number: {{ $usAct['acctnumber'] }}<br>Acct Name: {{ $usAct['firstname']}}&nbsp;{{ $usAct['lastname']}}</b></p>
+                                    </div>
+                                    <div>
+                                        <h5><span></span></h5>
+                                        <p><span>Click on "CONFIRM" after payment. We will recieve a notification and check the receipt of funds</span></p>
+                                    </div>
                                     @auth
-                                    @role('admin')
-                                    <div class="d-flex justify-content-center align-items-center">
-                                        <input type="text" name="orderId" value="{{ $sale->orderId }}" hidden='hidden'>
-                                        <input type="text" name="linkId" value="{{ $sale->id }}" hidden='hidden'>
-                                        {{-- <input type="submit" class="btn btn-primary px-4 me-3" value="Confirm" hidden='hidden'>  --}}
-                                    </div>
-                                    @endrole
-                                    @role('user')
-                                    <div class="d-flex justify-content-center align-items-center">
-                                        <input type="text" name="orderId" value="{{ $sale->orderId }}" hidden='hidden'>
-                                        <input type="text" name="linkId" value="{{ $sale->id }}" hidden='hidden'>
-                                        <input type="submit" class="btn btn-primary px-4 me-3" value="Confirm"> 
-                                    </div>
-                                    @endrole
-                                    @endauth
+                                        @role('admin')
+                                        <div class="d-flex justify-content-center align-items-center">
+                                            <input type="text" name="orderId" value="{{ $sale->orderId }}" hidden='hidden'>
+                                            <input type="text" name="linkId" value="{{ $sale->id }}" hidden='hidden'>
+                                            {{-- <input type="submit" class="btn btn-primary px-4 me-3" value="Confirm" hidden='hidden'>  --}}
+                                        </div>
+                                        @endrole
+                                        @role('user')
+                                        <div class="d-flex justify-content-center align-items-center">
+                                            <input type="text" name="orderId" value="{{ $sale->orderId }}" hidden='hidden'>
+                                            <input type="text" name="linkId" value="{{ $sale->id }}" hidden='hidden'>
+                                            <input type="submit" class="btn btn-success px-4 me-3" value="Confirm"> 
+                                        </div>
+                                        @endrole
+                                        @endauth
                                 </div>
-                            </div>
                             </form>
-                        </div>
-                    </div>
+                        </li>
+                    </ul>
                 </div>
             </div>
+            <!-- end row -->
         </div>
-
-    
     </div>
+    <!-- end card -->
+</div>
 </div>
 
+<style type="text/css">
+body{
+    background:#3a3361;
+    margin-top:20px;
+}
+.verti-timeline {
+    /* border-left: 3px dashed #e9ecef; */
+    margin: 0 10px;
+}
+.verti-timeline .event-list {
+    position: relative;
+    padding: 40px 15px 40px;
+    /* border-top: 3px solid #e9ecef; */
+}
+.verti-timeline .event-list .event-date {
+    position: absolute;
+    right: 0;
+    top: -2px;
+    padding: 12px;
+    color: #fff;
+    border-radius: 0 0 7px 7px;
+    text-align: center;
+}
+.verti-timeline .event-list .event-content {
+    position: relative;
+    border: 2px solid #e9ecef;
+    border-radius: 7px;
+    text-align: center;
+}
+.verti-timeline .event-list .event-content::before {
+    content: "";
+    position: absolute;
+    height: 40px;
+    width: 75%;
+    top: -42px;
+    left: 0;
+    right: 0;
+    margin: 0 auto;
+    /* border-left: 12px double #e9ecef;
+    border-right: 12px double #e9ecef; */
+}
+.verti-timeline .event-list .timeline-icon {
+    position: absolute;
+    left: -14px;
+    top: -10px;
+}
+.verti-timeline .event-list .timeline-icon i {
+    display: inline-block;
+    width: 23px;
+    height: 23px;
+    line-height: 23px;
+    font-size: 20px;
+    border-radius: 50%;
+    text-align: center;
+    color: #fff;
+}
+.verti-timeline .event-list:last-child {
+    padding-bottom: 0;
+}
+@media (min-width: 769px) {
+    .verti-timeline {
+        margin: 0 60px;
+    }
+    .verti-timeline .event-list {
+        padding: 40px 90px 60px 40px;
+    }
+}
+@media (max-width: 576px) {
+    .verti-timeline .event-list .event-date {
+        right: -20px;
+    }
+}
+.card {
+    border: none;
+    margin-bottom: 24px;
+    -webkit-box-shadow: 0 0 13px 0 rgba(236,236,241,.44);
+    box-shadow: 0 0 13px 0 rgba(236,236,241,.44);
+}
+</style>
 
-@endsection
+<script type="text/javascript">
+
+</script>
+</body>
+</html>
